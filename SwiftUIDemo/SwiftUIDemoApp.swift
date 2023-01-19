354fe3e43c0a5d495f00e59007a3d2ab90e7f0b2
Kevin Furjan <kevin.furjan@ericsson.com>
2023-01-19 14:53:42 +0100

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Ericsson on 19.01.2023..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
